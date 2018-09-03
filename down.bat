fa3d7fc7f7131fb9d9d3cce1e5d991c334b343e0
Joel <joel.flake@gmail.com>
2018-09-03 00:17:38 -0700

    Created testing package and began populating it

--- down.bat
+++ down.bat
@@ -1,3 +1,4 @@
 del /q "K:\Steam\steamapps\common\Fallout 4\Data\RecipeContainer.esp"
+del /q "K:\Steam\steamapps\common\Fallout 4\Data\RecipeContainerTesting.esp"
 call down-loose.bat
 call down-packed.bat
